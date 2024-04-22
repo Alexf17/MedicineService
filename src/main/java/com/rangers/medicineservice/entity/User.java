31107fa5a2d76fed4d51513b517e2f5f96a4a977
Viktor Bulatov <lightoton@gmail.com>
2024-04-23 00:45:27 +0200

    added entity User and dependency

---
+++ src/main/java/com/rangers/medicineservice/entity/User.java
@@ -0,0 +1,48 @@
+package com.rangers.medicineservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "chatId")
+    private String chatId;
+
+    @Column(name = "healthTicket")
+    private String healthTicket;
+
+    @OneToMany
+    List<Recept> recepts;
+
+    @OneToMany
+    List<Schedule> schedule;
+}
