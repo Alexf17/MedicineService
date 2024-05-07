b702eaaac67b564f0010282a01e95c71414b66d4
pamail08@gmail.com <pamail08@gmail.com>
2024-05-07 19:39:48 +0200

    changes in UserControllerTest

--- doctorsAndPharmacy/src/main/java/com/rangers/medicineservice/repository/UserRepository.java
+++ doctorsAndPharmacy/src/main/java/com/rangers/medicineservice/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     User getUserByChatId(String chatId);
+    User getUserByPolicyNumber(String policyNumber);
 }
